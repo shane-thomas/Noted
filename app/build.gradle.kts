e68e7b8204bafe7dbbddc4aa80a038b4d4672ec8
Shane Thomas <shaneshaji10@gmail.com>
2026-01-30 23:34:38 +0300

    feat: added a m3e toolbar

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,6 +41,7 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
+    implementation(libs.androidx.material3)
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.graphics)
---
+++ app/src/main/java/io/github/shane/thomas/noted/ui/components/EditorToolbar.kt
@@ -0,0 +1,109 @@
+package io.github.shane.thomas.noted.ui.components
+
+import androidx.compose.foundation.layout.imePadding
+import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
+import androidx.compose.material3.FloatingToolbarDefaults
+import androidx.compose.material3.FloatingToolbarExitDirection.Companion.Bottom
+import androidx.compose.material3.HorizontalFloatingToolbar
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.PlainTooltip
+import androidx.compose.material3.Text
+import androidx.compose.material3.TooltipAnchorPosition
+import androidx.compose.material3.TooltipBox
+import androidx.compose.material3.TooltipDefaults
+import androidx.compose.material3.rememberTooltipState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.zIndex
+import io.github.shane.thomas.noted.R
+
+@OptIn(ExperimentalMaterial3ExpressiveApi::class)
+@Composable
+@Preview
+fun EditorToolbar(modifier: Modifier = Modifier) {
+    val exitAlwaysScrollBehavior = FloatingToolbarDefaults.exitAlwaysScrollBehavior(Bottom)
+    var expanded by remember { mutableStateOf(false) }
+    HorizontalFloatingToolbar(
+        expanded = expanded,
+        colors = FloatingToolbarDefaults.vibrantFloatingToolbarColors(),
+        scrollBehavior = exitAlwaysScrollBehavior,
+        floatingActionButton = {
+            TooltipBox(
+                positionProvider = TooltipDefaults.rememberTooltipPositionProvider(
+                    TooltipAnchorPosition.Above
+                ),
+                tooltip = { PlainTooltip { Text("Open new note") } },
+                state = rememberTooltipState(),
+            ) {
+
+                FloatingToolbarDefaults.VibrantFloatingActionButton(
+                    onClick = { expanded = !expanded }) {
+                    Icon(painter = painterResource(R.drawable.add_icon), contentDescription = "Add")
+                }
+            }
+        },
+        modifier = modifier
+            .imePadding()
+            .zIndex(1f),
+        content = {
+            TooltipBox(
+                positionProvider = TooltipDefaults.rememberTooltipPositionProvider(
+                    TooltipAnchorPosition.Above
+                ),
+                tooltip = { PlainTooltip { Text("Bold") } },
+                state = rememberTooltipState(),
+            ) {
+                IconButton(onClick = { /* doSomething() */ }) {
+                    Icon(painter = painterResource(R.drawable.add_icon), contentDescription = "Add")
+
+                }
+            }
+            TooltipBox(
+                positionProvider = TooltipDefaults.rememberTooltipPositionProvider(
+                    TooltipAnchorPosition.Above
+                ),
+                tooltip = { PlainTooltip { Text("Italic") } },
+                state = rememberTooltipState(),
+            ) {
+                IconButton(onClick = { /* doSomething() */ }) {
+                    Icon(painter = painterResource(R.drawable.add_icon), contentDescription = "Add")
+                }
+            }
+            TooltipBox(
+                positionProvider = TooltipDefaults.rememberTooltipPositionProvider(
+                    TooltipAnchorPosition.Above
+                ),
+                tooltip = { PlainTooltip { Text("Heading") } },
+                state = rememberTooltipState(),
+            ) {
+                IconButton(onClick = { /* doSomething() */ }) {
+                    Icon(painter = painterResource(R.drawable.add_icon), contentDescription = "Add")
+
+                }
+            }
+
+            TooltipBox(
+                positionProvider = TooltipDefaults.rememberTooltipPositionProvider(
+                    TooltipAnchorPosition.Above
+                ),
+                tooltip = { PlainTooltip { Text("Checkbox") } },
+                state = rememberTooltipState(),
+            ) {
+                IconButton(onClick = { /* doSomething() */ }) {
+                    Icon(painter = painterResource(R.drawable.add_icon), contentDescription = "Add")
+
+                }
+            }
+
+        },
+    )
+
+}
+
